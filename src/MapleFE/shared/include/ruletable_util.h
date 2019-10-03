f1f0ffed879be421a5e1b729f05499419526715f
yehandong <yehandong@gmail.com>
2019-10-02 23:39:15 -0700

    Add ruletable_util.h, which contains the validity checking functions for each rule. 	new file:   shared/include/ruletable_util.h

---
+++ src/MapleFE/shared/include/ruletable_util.h
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////
+// This file contains all the functions that are used when we traverse the
+// rule tables. Most of the functions are designed to do the following
+// 1. Validity check
+//    e.g. if an identifier is a type name, variable name, ...
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef __RULE_TABLE_UTIL_H__
+#define __RULE_TABLE_UTIL_H__
+
+// This class contains all the functions that a language could use during parsing.
+// The mechansim behind this design has three major parts.
+//
+//   1. ValidityCheck is the parent class of the children classes, say JavaValidityCheck.
+//      ValidityCheck has some common implementations, and children can have their own.
+//
+//   2. Children's validity checking is language specific, eg Java has a requirement to
+//      check if an identifier is a packagename. The package names are specific in each
+//      language.
+//
+//   3. The hard problem is how to invoke the correct functions when a rule is matched.
+//      The most straightforward idea is to put the function pointers in the rule table
+//      with additional information. However, the signatures of a function makes the idea
+//      complicated.
+//      
+//      So I finally decided to have an Id for each check function, and Id is written into
+//      the rule tables by Autogen.
+
+class ValidityCheck {
+public:
+  virtual bool IsPackageName();
+  virtual bool IsTypeName();
+  virtual bool IsVariable();
+};
+
+#endif
