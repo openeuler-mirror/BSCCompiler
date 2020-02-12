ea78566946a51b45d3000078dda092f4646a8d91
ye_handong <yehandong@gmail.com>
2020-02-11 22:45:09 -0800

    Fixed the license in some files. 	new file:   scripts/generate-result.sh 	modified:   scripts/prepend-copyright.sh 	modified:   test/Makefile 	deleted:    test/java2mpl/generate-result.sh 	modified:   test/testall

--- src/MapleFE/scripts/generate-result.sh
+++ src/MapleFE/scripts/generate-result.sh
@@ -13,11 +13,9 @@
 # 
 
 #!/bin/bash
-FILES=$(pwd)/*.cpp
+FILES=$(pwd)/*.java
 for f in $FILES
 do
-  echo "Generating copyright for $f ..."
-  cp ~/MapleFE/Copyright.orig ~/MapleFE/Copyright
-  cat $f >> ~/MapleFE/Copyright
-  cp ~/MapleFE/Copyright $f
+  echo "Generating result for $f ..."
+  ../../build64/java/java2mpl $f > $f.result
 done
--- src/MapleFE/test/java2mpl/generate-result.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILES=$(pwd)/*.java
-for f in $FILES
-do
-  echo "Generating result for $f ..."
-  ../../build64/java/java2mpl $f > $f.result
-done
