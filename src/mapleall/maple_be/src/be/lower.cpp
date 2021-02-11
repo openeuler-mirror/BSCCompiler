a081e03ebc7052e650f50013e9505ea7f98cf188
Alfred Huang <ahuang2@futurewei.com>
2021-02-11 14:39:40 -0800

    1. Change all label defs/access from name based to puidx based. 2, Support pstatic data for C/C++. 3. Enhance string constant functions. 4. Fix be/lower.cpp handling of C_va_start intrinsic.

--- src/mapleall/maple_be/src/be/lower.cpp
+++ src/mapleall/maple_be/src/be/lower.cpp
@@ -1186,7 +1186,8 @@ StmtNode *CGLowerer::LowerCall(CallNode &callNode, StmtNode *&nextStmt, BlockNod
     MIRPtrType *pretType = static_cast<MIRPtrType*>((calleeFunc->GetNthParamType(0)));
     CHECK_FATAL(pretType != nullptr, "nullptr is not expected");
     retType = GlobalTables::GetTypeTable().GetTypeFromTyIdx(pretType->GetPointedTyIdx());
-    CHECK_FATAL((retType->GetKind() == kTypeStruct) || (retType->GetKind() == kTypeUnion), "make sure retType is a struct type");
+    CHECK_FATAL((retType->GetKind() == kTypeStruct) || (retType->GetKind() == kTypeUnion),
+        "make sure retType is a struct type");
   }
 
   /* if return type is not of a struct, return */
@@ -2508,6 +2509,9 @@ StmtNode *CGLowerer::LowerIntrinsiccall(IntrinsiccallNode &intrincall, BlockNode
   }
   if (intrnID == INTRN_MPL_CLEAR_STACK) {
     return LowerIntrinsicMplClearStack(intrincall, newBlk);
+  }
+  if (intrnID == INTRN_C_va_start) {
+    return &intrincall;
   }
   IntrinDesc *intrinDesc = &IntrinDesc::intrinTable[intrnID];
   if (intrinDesc->IsSpecial()) {
