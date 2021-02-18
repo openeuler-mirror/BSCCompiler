75890fe2b8d8707d134529c3686313e688ba3947
Fred Chow <fred.chow@yandex.com>
2021-02-17 22:46:45 -0800

    perform copy propagation in the irmap_build phase

---
+++ src/mapleall/maple_me/include/me_prop.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) [2020] Huawei Technologies Co.,Ltd.All rights reserved.
+ *
+ * OpenArkCompiler is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *     http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+#ifndef MAPLE_ME_INCLUDE_MEPROP_H
+#define MAPLE_ME_INCLUDE_MEPROP_H
+#include "me_irmap.h"
+#include "bb.h"
+#include "me_phase.h"
+#include "prop.h"
+
+namespace maple {
+class MeProp : public Prop {
+ public:
+  MeProp(MeIRMap &irMap, Dominance &dom, MemPool &memPool, const PropConfig &config)
+      : Prop(irMap, dom, memPool, irMap.GetFunc().GetAllBBs().size(), config),
+        func(&irMap.GetFunc()) {}
+
+  virtual ~MeProp() = default;
+ private:
+  MeFunction *func;
+
+  BB *GetBB(BBId id) {
+    return func->GetAllBBs()[id];
+  }
+};
+
+class MeDoMeProp : public MeFuncPhase {
+ public:
+  explicit MeDoMeProp(MePhaseID id) : MeFuncPhase(id) {}
+
+  virtual ~MeDoMeProp() = default;
+  AnalysisResult *Run(MeFunction *func, MeFuncResultMgr *m, ModuleResultMgr *mrm) override;
+  std::string PhaseName() const override {
+    return "hprop";
+  }
+};
+}  // namespace maple
+#endif  // MAPLE_ME_INCLUDE_MEPROP_H
