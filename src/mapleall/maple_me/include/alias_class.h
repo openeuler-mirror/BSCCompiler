c3ceca6f5d53db45cc47ea7c17e14041caa20c61
Fred Chow <fredchow@gitee.com>
2021-04-27 08:49:40 -0700

    implemented FindOrCreateAliasElemOfAddrofZeroFieldIDOSt() so the OriginalSt created can cover entire struct

--- src/mapleall/maple_me/include/alias_class.h
+++ src/mapleall/maple_me/include/alias_class.h
@@ -191,8 +191,10 @@ class AliasClass : public AnalysisResult {
   AliasElem *FindOrCreateDummyNADSAe();
   bool IsPointedTo(OriginalSt &oSt);
   AliasElem &FindOrCreateAliasElemOfAddrofOSt(OriginalSt &oSt);
+  AliasElem &FindOrCreateAliasElemOfAddrofZeroFieldIDOSt(OriginalSt &oSt);
   void CollectMayDefForMustDefs(const StmtNode &stmt, std::set<OriginalSt*> &mayDefOsts);
-  void CollectMayUseForNextLevel(OriginalSt *ost, std::set<OriginalSt*> &mayUseOsts, const StmtNode &stmt, bool isFirstOpnd);
+  void CollectMayUseForNextLevel(const OriginalSt *ost, std::set<OriginalSt*> &mayUseOsts,
+                                 const StmtNode &stmt, bool isFirstOpnd);
   void CollectMayUseForCallOpnd(const StmtNode &stmt, std::set<OriginalSt*> &mayUseOsts);
   void InsertMayDefNodeForCall(std::set<OriginalSt*> &mayDefOsts, TypeOfMayDefList &mayDefNodes,
                                StmtNode &stmt, BBId bbid, bool hasNoPrivateDefEffect);
