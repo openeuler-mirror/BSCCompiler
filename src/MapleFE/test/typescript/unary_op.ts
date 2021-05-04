b60fb3905fbb50805127ec69b8e582e40a327246
Wen HU <git_huwen@yahoo.com>
2021-05-04 19:27:10 -0400

    add case unary_op.ts

---
+++ src/MapleFE/test/typescript/unary_op.ts
@@ -0,0 +1,28 @@
+class A {
+  a: number;
+  b: number;
+}
+var x : A;
+delete x.a;
+
+void 0
+
+typeof 8;
+
+var a : number = 8;
+++a;
+--a;
+
++8;
++"-8";
+
+-"8";
+-a;
+
+~8;
+~"8";
+~a;
+
+!false;
+!true;
+
