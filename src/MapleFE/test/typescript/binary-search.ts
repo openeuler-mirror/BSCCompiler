85cfd7e6ee678989a67c5b33dc957d5292bb40c4
Ed Ching <eching@futurewei.com>
2021-05-04 16:41:12 -0700

    Simplified testcase. 	modified:   binary-search.ts

--- src/MapleFE/test/typescript/binary-search.ts
+++ src/MapleFE/test/typescript/binary-search.ts
@@ -1,37 +1,19 @@
-/* @param array The array to search in.
- * @param value The value to search.
- * @return The index of the searched element in the sorted array, if one is found;
- * otherwise, a negative number that is the bitwise complement of the index of the next element that is large than the searched value or,
- * if there is no larger element(include the case that the array is empty), the bitwise complement of array's length.
- */
-function binarySearch (array: number[], value: number) {
-    return binarySearchEpsilon(array, value, 0);
-}
 
-/**
- * Searches the **sorted** number array for an element and returns the index of that element.
- * @param array The array to search in.
- * @param value The value to search.
- * @param EPSILON The epsilon to compare the numbers. Default to `1e-6`.
- * @return The index of the searched element in the sorted array, if one is found;
- * otherwise, a negative number that is the bitwise complement of the index of the next element that is large than the searched value or,
- * if there is no larger element(include the case that the array is empty), the bitwise complement of array's length.
- */
-function binarySearchEpsilon (array: number[], value: number, EPSILON = 1e-6) {
-    let low = 0;
-    let high = array.length - 1;
-    let middle = high >>> 1;
-    for (; low <= high; middle = (low + high) >>> 1) {
-        const test = array[middle];
-        if (test > (value + EPSILON)) {
-            high = middle - 1;
-        } else if (test < (value - EPSILON)) {
-            low = middle + 1;
-        } else {
-            return middle;
-        }
+function binarySearch (array: number[], value: number) {
+  var low: number = 0;
+  var high: number = array.length - 1;
+  var mid: number = high >>> 1;
+  for (; low <= high; mid = (low + high) >>> 1) {
+    const test = array[mid];
+    if (test > value) {
+      high = mid - 1;
+    } else if (test < value) {
+      low = mid + 1;
+    } else {
+      return mid;
     }
-    return ~low;
+  }
+  return ~low;
 }
 
 var sequence: number[] = [13, 21, 34, 55, 89, 144];
