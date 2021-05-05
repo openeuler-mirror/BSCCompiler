21bb6f5fc4e0fbbdf5b93110b7a143324fef0fdb
Yan Zhang <yzhang@futurewei.com>
2021-05-05 13:08:09 -0700

    Add semicolons manually since automatic semicolon insertion is not done yet

--- src/MapleFE/test/typescript/switch-in-loop.ts
+++ src/MapleFE/test/typescript/switch-in-loop.ts
@@ -2,18 +2,18 @@ for (var i: number = 1; i < 10; ++i) {
   if(i < 5)
     switch(i) {
       case 2:
-        console.log(i, ", continue")
-        continue
+        console.log(i, ", continue");
+        continue;
       case 3:
-        console.log(i, ", fall-through")
+        console.log(i, ", fall-through");
       case 4:
-        console.log(i, ", break")
-        break
+        console.log(i, ", break");
+        break;
       default:
-        console.log(i, ", default")
+        console.log(i, ", default");
     }
     else {
-      console.log(i, ", else branch")
-      break
+      console.log(i, ", else branch");
+      break;
     }
 }
