6a98e647d1bf9e5da7dfee6083d01ce5eb6463b6
Yan Zhang <yzhang@futurewei.com>
2021-05-12 13:29:48 -0700

    Add an option -s|--syntax to syntax-highlight the generated TypeScript code

--- src/MapleFE/test/astdump.sh
+++ src/MapleFE/test/astdump.sh
@@ -9,12 +9,13 @@ Usage: astdump.sh [-dot] [-f|--fullscreen] [-p <PREFIX>|--pre <PREFIX>] [-a|--as
   -p | --pre <PREFIX>    Filter graphs with the specified <PREFIX>, e.g. -p "CFG_<function-name>"
   -a | --ast             Show AST graph. It is equivalent to options "-dot -p AST"
   -c | --cfg             Show CFG graph. It is equivalent to options "-dot -p CFG"
+  -s | --syntax          Syntax highlighting the generated TypeScript code
   <file1> [<file2> ...]  Specify one or more TypeScript files to be processed
 EOF
 exit 1
 }
 
-DOT= PRE= LIST= VIEWOP=
+DOT= PRE= LIST= VIEWOP= HIGHLIGHT="cat"
 while [ $# -gt 0 ]; do
     case $1 in
         -d|-dot|--dot)   DOT=true;;
@@ -22,13 +23,14 @@ while [ $# -gt 0 ]; do
         -p|-pre|--pre)   [ $# -ge 2 ] && { PRE="$2"; shift; } || { echo "$1 needs an argument"; exit 1; } ;;
         -a|-ast|--ast)   PRE="AST" ; DOT=true ;;
         -c|-cfg|--cfg)   PRE="CFG" ; DOT=true ;;
+        -s|--syntax)     HIGHLIGHT="highlight -O xterm256 --syntax ts" ;;
         -*)              usage;;
         *)               LIST="$LIST $1"
     esac
     shift
 done
 [ -n "$LIST" ] || { echo Please specify one or more TypeScript files.; usage; }
-[ -z "$DOT" ] || [ -x /usr/bin/dot -a -x /usr/bin/viewnior ] || sudo apt install graphviz viewnior 
+[ -z "$DOT" ] || [ -x /usr/bin/dot -a -x /usr/bin/viewnior -x /usr/bin/highlight ] || sudo apt install graphviz viewnior highlight
 CMD=$(dirname $0)/../output/typescript/typescript/ts2cpp
 [ -x "$CMD" ] || { echo Cannot execute $CMD; exit 1; }
 Failed=
@@ -39,7 +41,7 @@ for ts in $LIST; do
   echo "$out"
   cmd=$(grep -n -e "^// .Beginning of AstEmitter:" -e "^// End of AstEmitter.$" <<< "$out" |
     tail -2 | sed 's/:.*//' | xargs | sed 's/\([^ ]*\) \(.*\)/sed -n \1,$((\2+1))p/')
-  [ -n "$cmd" ] && { echo -e "\n====== Reformated ======\n"; eval $cmd <<<"$out" | clang-format-10; }
+  [ -n "$cmd" ] && { echo -e "\n====== Reformated ======\n"; eval $cmd <<<"$out" | clang-format-10 | $HIGHLIGHT; }
   grep -n -e "^digraph $PRE[^{]* {" -e "^}" <<< "$out" | grep -A1 "digraph [^{]* {" |
   if [ -n "$DOT" ]; then
     grep -n -e "^digraph $PRE[^{]* {" -e "^}" <<< "$out" | grep -A1 "digraph [^{]* {" |
