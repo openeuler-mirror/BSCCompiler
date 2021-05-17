5ae90bb47131c13774bbcf98c1d0ae41819ad389
Yan Zhang <yzhang@futurewei.com>
2021-05-17 13:53:15 -0700

    Rule out the issue of var declaration for multile vars to make this case focus on only the comma issue

--- src/MapleFE/test/typescript/unit_tests/comma.ts
+++ src/MapleFE/test/typescript/unit_tests/comma.ts
@@ -1,5 +1,7 @@
 var arr: number[] = [7, 4, 5, 9, 2, 8, 1, 6, 3];
-var sum: number = 0, i, len;
+var sum: number = 0;
+var i;
+var len;
 i = 0, len = arr.length;
 for (; i < len; ++i) {
   sum += arr[i];
