67b014d331e9e9f491af9457dd03aecf8f6dcd61
linma <lma1@futurewei.com>
2021-05-28 11:23:23 -0700

    OAC merge: merge phase injectIV from incubator, this is first step of lfo optmization and only enabled with -O3

--- src/mapleall/maple_me/src/me_cfg.cpp
+++ src/mapleall/maple_me/src/me_cfg.cpp
@@ -1478,6 +1478,15 @@ void MeCFG::CreateBasicBlocks() {
             SetBBTryNodeMap(*newBB, *tryStmt);
           }
           curBB = newBB;
+        } else if (func.GetLfoFunc() &&
+                    (func.GetLfoFunc()->label2WhileInfo.find(labelIdx) != func.GetLfoFunc()->label2WhileInfo.end())) {
+          curBB->SetKind(kBBFallthru);
+          BB *newBB = NewBasicBlock();
+          if (tryStmt != nullptr) {
+            newBB->SetAttributes(kBBAttrIsTry);
+            SetBBTryNodeMap(*newBB, *tryStmt);
+          }
+          curBB = newBB;
         }
         labelBBIdMap[labelIdx] = curBB;
         curBB->SetBBLabel(labelIdx);
@@ -1690,7 +1699,7 @@ void MeCFG::BuildSCC() {
   SCCTopologicalSort(sccNodes);
 }
 
-AnalysisResult *MeDoMeCfg::Run(MeFunction *func, MeFuncResultMgr *m, ModuleResultMgr *mrm) {
+AnalysisResult *MeDoMeCfg::Run(MeFunction *func, MeFuncResultMgr*, ModuleResultMgr*) {
   MemPool *meCfgMp = NewMemPool();
   MeCFG *theCFG = meCfgMp->New<MeCFG>(meCfgMp, *func);
   func->SetTheCfg(theCFG);
@@ -1708,21 +1717,6 @@ AnalysisResult *MeDoMeCfg::Run(MeFunction *func, MeFuncResultMgr *m, ModuleResul
   theCFG->UnreachCodeAnalysis();
   theCFG->WontExitAnalysis();
   theCFG->Verify();
-#if 0  // these 2 phases will be invoked by phase manager
-  if (mrm == nullptr) {
-    MeDoLoopCanon doLoopCanon(MeFuncPhase_LOOPCANON);
-    if (!MeOption::quiet) {
-      LogInfo::MapleLogger() << "  == " << PhaseName() << " invokes [ " << doLoopCanon.PhaseName() << " ] ==\n";
-    }
-    doLoopCanon.Run(func, m, NULL);
-
-    MeDoSplitCEdge doSplitCEdge(MeFuncPhase_SPLITCEDGE);
-    if (!MeOption::quiet) {
-      LogInfo::MapleLogger() << "  == " << PhaseName() << " invokes [ " << doSplitCEdge.PhaseName() << " ] ==\n";
-    }
-    doSplitCEdge.Run(func, m, NULL);
-  }
-#endif
   return theCFG;
 }
 }  // namespace maple
