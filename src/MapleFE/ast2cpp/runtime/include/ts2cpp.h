7adb5795ab6010eafc6cd65afd5380acd3944f8e
Yan Zhang <yzhang@futurewei.com>
2021-06-04 07:31:16 -0700

    Initial checkin of ts2cpp header for TS2CPP runtime

---
+++ src/MapleFE/ast2cpp/runtime/include/ts2cpp.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) [2021] Futurewei Technologies, Inc. All rights reverved.
+ *
+ * OpenArkFE is licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *  http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef __TS2CPP_RT_HEADER__
+#define __TS2CPP_RT_HEADER__
+
+namespace t2crt {
+
+} // namespace t2crt
+#endif
