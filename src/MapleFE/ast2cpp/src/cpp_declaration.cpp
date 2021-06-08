6047674c5fbb40cfa934c819e5220a9eadf012e3
Yan Zhang <yzhang@futurewei.com>
2021-06-08 15:52:38 -0700

    Update the comment for TypeScript filename

--- src/MapleFE/ast2cpp/src/cpp_declaration.cpp
+++ src/MapleFE/ast2cpp/src/cpp_declaration.cpp
@@ -25,7 +25,7 @@ std::string CppDecl::EmitModuleNode(ModuleNode *node) {
   for(auto &c : name)
     header += std::toupper(c);
   header += "__HEADER__\n";
-  std::string str("// Filename: "s + node->GetFileName() + "\n"s);
+  std::string str("// TypeScript filename: "s + node->GetFileName() + "\n"s);
   str += "#ifndef "s + header + "#define "s + header;
   str += R"""(
 //#include "ts2cpp.h"
