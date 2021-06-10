f328f0df576225b91ca901858ce6954e1ccb4f41
eching <edward.k.ching@gmail.com>
2021-06-10 13:26:25 -0700

    Added testcase for TypeScript non-null assertion operator. This is Typescript 2.0 but is used in cocos director.ts. 	new file:   non-null-assertion.ts

---
+++ src/MapleFE/test/typescript/unit_tests/non-null-assertion.ts
@@ -0,0 +1,5 @@
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+// used in cocos director.ts
+function processEntity(e?: any) {
+  let s = e!.name; // Assert that e is non-null and access name
+}
