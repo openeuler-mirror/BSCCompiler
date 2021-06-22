dca238943dd457d46ff11276b8df85b2097f7568
linma <lma1@futurewei.com>
2021-06-22 10:06:29 -0700

    refine LFO IR: map parent node and hssa info to mirnode and change lfopreemit phase to analysis phase

--- src/mapleall/maple_me/include/lfo_pre_emit.h
+++ src/mapleall/maple_me/include/lfo_pre_emit.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) [2020] Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (c) [2021] Huawei Technologies Co., Ltd. All rights reserved.
  *
  * OpenArkCompiler is licensed under the Mulan Permissive Software License v2.
  * You can use this software according to the terms and conditions of the MulanPSL - 2.0.
@@ -19,37 +19,68 @@
 #include "me_phase.h"
 
 namespace maple {
-
-class LfoPreEmitter {
+class LfoPreEmitter : public AnalysisResult {
  private:
   MeIRMap *meirmap;
   LfoFunction *lfoFunc;
   MIRFunction *mirFunc;
   MemPool *codeMP;
   MapleAllocator *codeMPAlloc;
+  MemPool *lfoMP;
+  MapleAllocator lfoMPAlloc;
+  MapleMap<BaseNode*, LfoPart*> lfoParts; // key is mirnode
   MeCFG *cfg;
 
  public:
-  LfoPreEmitter(MeIRMap *hmap, LfoFunction *f) : meirmap(hmap),
-    lfoFunc(f),
-    mirFunc(f->meFunc->GetMirFunc()),
-    codeMP(f->meFunc->GetMirFunc()->GetCodeMempool()),
-    codeMPAlloc(&f->meFunc->GetMirFunc()->GetCodeMemPoolAllocator()),
-    cfg(f->meFunc->GetCfg()) {}
+  LfoPreEmitter(MeIRMap *hmap, LfoFunction *f, MemPool *lfomp)
+      : AnalysisResult(lfomp), meirmap(hmap),
+        lfoFunc(f),
+        mirFunc(f->meFunc->GetMirFunc()),
+        codeMP(f->meFunc->GetMirFunc()->GetCodeMempool()),
+        codeMPAlloc(&f->meFunc->GetMirFunc()->GetCodeMemPoolAllocator()),
+        lfoMP(lfomp),
+        lfoMPAlloc(lfoMP),
+        lfoParts(lfoMPAlloc.Adapter()),
+        cfg(f->meFunc->GetCfg()) {}
 
  private:
-  LfoParentPart *EmitLfoExpr(MeExpr*, LfoParentPart *);
-  StmtNode* EmitLfoStmt(MeStmt *, LfoParentPart *);
-  void EmitBB(BB *, LfoBlockNode *);
-  DoloopNode *EmitLfoDoloop(BB *, LfoBlockNode *, LfoWhileInfo *);
-  WhileStmtNode *EmitLfoWhile(BB *, LfoBlockNode *);
-  uint32 Raise2LfoWhile(uint32, LfoBlockNode *);
-  uint32 Raise2LfoIf(uint32, LfoBlockNode *);
+  BaseNode *EmitLfoExpr(MeExpr*, BaseNode *);
+  StmtNode* EmitLfoStmt(MeStmt *, BaseNode *);
+  void EmitBB(BB *, BlockNode *);
+  DoloopNode *EmitLfoDoloop(BB *, BlockNode *, LfoWhileInfo *);
+  WhileStmtNode *EmitLfoWhile(BB *, BlockNode *);
+  uint32 Raise2LfoWhile(uint32, BlockNode *);
+  uint32 Raise2LfoIf(uint32, BlockNode *);
  public:
-  uint32 EmitLfoBB(uint32, LfoBlockNode *);
+  uint32 EmitLfoBB(uint32, BlockNode *);
+  void InitFuncBodyLfoPart(BaseNode *funcbody) {
+    LfoPart *rootlfo = lfoMP->New<LfoPart>(nullptr);
+    lfoParts[funcbody] = rootlfo;
+  }
+  void SetLfoPart(BaseNode* node, LfoPart* lfoInfo) {
+     lfoParts[node] = lfoInfo;
+  }
+  LfoPart* GetLfoPart(BaseNode *node) {
+    return lfoParts[node];
+  }
+  BaseNode *GetParent(BaseNode *node) {
+    LfoPart *lfopart = lfoParts[node];
+    if (lfopart != nullptr) {
+      return lfopart->parent;
+    }
+    return nullptr;
+  }
+  MeExpr *GetMexpr(BaseNode *node) {
+    LfoPart *lfopart = lfoParts[node];
+    return lfopart->meexpr;
+  }
+  MeStmt *GetMeStmt(BaseNode *node) {
+    LfoPart *lfopart = lfoParts[node];
+    return lfopart->mestmt;
+  }
 };
 
-/*emit ir to specified file*/
+/* emit ir to specified file */
 class DoLfoPreEmission : public MeFuncPhase {
  public:
   DoLfoPreEmission(MePhaseID id) : MeFuncPhase(id) {}
@@ -57,6 +88,5 @@ class DoLfoPreEmission : public MeFuncPhase {
   AnalysisResult *Run(MeFunction *func, MeFuncResultMgr *m, ModuleResultMgr *mrm) override;
   std::string PhaseName() const override { return "lfopreemit"; }
 };
-
 }  // namespace maple
 #endif  // MAPLE_ME_INCLUDE_LFO_PRE_EMIT_H
