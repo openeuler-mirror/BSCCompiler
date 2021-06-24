85e86832bc0b6a5cfd151a35886007719111b7b0
Yan Zhang <yzhang@futurewei.com>
2021-06-23 22:23:44 -0700

    Generate no node clusters for graph with constructor edges

--- src/MapleFE/test/typescript/utils/proto.js
+++ src/MapleFE/test/typescript/utils/proto.js
@@ -11,20 +11,21 @@ class MyCar extends Car {
 let car = new Car("Tesla");
 let mycar = new MyCar("Tesla Model X");
 
-for(g = 0; g < 2; ++g) { 
+for(let g = 0; g < 2; ++g) {
   let arr = []
   let objs =  [  arr,   Array,   Vehicle,   Car,   MyCar,   car,   mycar,   Function,   Object,  ];
   let names = [ "arr", "Array", "Vehicle", "Car", "MyCar", "car", "mycar", "Function", "Object", ];
-  console.log("digraph JS" + g + " {\nnewrank=true;\nnodesep=1;\nranksep=1;");
+  console.log("digraph JS" + g + " {\nranksep=0.6;\nnodesep=0.6;\n" + (g == 0 ? "newrank=true;\n" : ""));
   let num = objs.length;
   let k = num;
   for(let i = 0; i < num; ++i) {
-    x = typeof objs[i].prototype;
+    let x = typeof objs[i].prototype;
     if(x === "function" || x === "object") {
       objs[k] = objs[i].prototype;
       names[k] = names[i] + "_prototype";
-      console.log("subgraph cluster_" + names[i] + " {\nrank=same;\ncolor=white;\n" + names[i] + ";");
-      console.log(names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];\n }");
+      console.log(g == 0 ? "subgraph cluster_" + names[i] + " {\nrank=same;\ncolor=white;\n"
+        + names[i] + ";\n" + names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];\n }"
+        : names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];");
       console.log(names[i] + " -> " + names[k] + " [label=\"prototype\", color=blue, fontcolor=blue];");
       k++;
     }
