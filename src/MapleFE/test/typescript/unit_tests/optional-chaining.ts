6479163636a299ca01b969d1c29c569ec74f44e8
Yan Zhang <yzhang@futurewei.com>
2021-06-25 14:48:05 -0700

    Add a test case for optional chaining operator

--- src/MapleFE/test/typescript/unit_tests/optional-chaining.ts
+++ src/MapleFE/test/typescript/unit_tests/optional-chaining.ts
@@ -1,10 +1,8 @@
-// Javascript optional chaining operator. Used in Cocos Ceator class.ts
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
-// Requires nodejs v.14.0.0 and above.
-// https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04
+function func(arr?: number[]) {
+  return arr?.[0];
+}
 
-let obj1 = {};  // has hasOwnProperty prop
-let obj2;       // does not have hasOwnProperty prop
-console.log(obj1.hasOwnProperty('prop1'));
-console.log(obj2?.hasOwnProperty('prop1'));
+let arr : number[] = [ 1, 2, 3];
+console.log(func(arr));
+console.log(func());
 
