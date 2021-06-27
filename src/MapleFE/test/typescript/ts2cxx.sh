2965ee99400868e4b190ee8521f5ad0af4b93c2e
Yan Zhang <yzhang@futurewei.com>
2021-06-26 23:39:20 -0700

    Add ts2cxx.sh to test C++ emitter with TypeScript test cases

---
+++ src/MapleFE/test/typescript/ts2cxx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+# Usage: cd MapleFE/test/typescript/unit_tests; ../ts2cpp.sh *.ts
+SUCC=
+log=cxx.log
+for f; do
+  t=$(basename $f .ts)
+  [ -f $t.ts ] && f=$t.ts
+  ts2ast $f || continue
+  ast2cpp $f.ast || continue
+  g++ $t.cpp -o $t || continue
+  ./$t || continue
+  SUCC="$SUCC $t"
+done 2>&1 
+echo "$SUCC" | xargs -n1 | nl | tee -a $log
