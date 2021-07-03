1702e48a2c73d31bd7d17a7ed8d258ddd1ebd2c1
eching <edward.k.ching@gmail.com>
2021-07-02 17:17:26 -0700

    Added defintions for JS builtins. 	new file:   src/builtins.cpp

---
+++ src/MapleFE/ast2cpp/runtime/src/builtins.cpp
@@ -0,0 +1,11 @@
+#include "ts2cpp.h"
+
+namespace t2crt {
+
+BaseObj    Object_prototype  ((Ctor *)&Object_ctor,   nullptr);
+BaseObj    Function_prototype((Ctor *)&Function_ctor, &Object_prototype);
+Ctor_Function Function_ctor(&Function_ctor, &Function_prototype, &Function_prototype);
+Ctor_Object   Object_ctor  (&Function_ctor, &Function_prototype, &Object_prototype);
+
+} // namepsace t2crt
+
