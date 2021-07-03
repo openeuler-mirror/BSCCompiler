4be8358e6bf29c241ec007771b78be0f29e78e3e
eching <edward.k.ching@gmail.com>
2021-07-02 17:14:39 -0700

    Moved stream output operator function defintion from ts2cpp.h to ts2cpp.cpp so ts2cpp.h can be mulitpily included. 	modified:   include/ts2cpp.h 	new file:   src/ts2cpp.cpp

--- src/MapleFE/ast2cpp/runtime/include/ts2cpp.h
+++ src/MapleFE/ast2cpp/runtime/include/ts2cpp.h
@@ -196,23 +196,6 @@ std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
   return out;
 }
 
-std::ostream& operator<< (std::ostream& out, const t2crt::JS_Val& v) {
-  switch(v.type) {
-    case t2crt::TY_None: out << "None"; break;
-    case t2crt::TY_Undef: out << "undefined"; break;
-    case t2crt::TY_Null: out << "null"; break;
-    case t2crt::TY_Bool: out << v.x.val_bool; break;
-    case t2crt::TY_Long: out << v.x.val_long; break;
-    case t2crt::TY_Double: out << v.x.val_double; break;
-    case t2crt::TY_BigInt: out << "bigint"; break;
-    case t2crt::TY_String: out << "string"; break;
-    case t2crt::TY_Symbol: out << "symbol"; break;
-    case t2crt::TY_Function: out << "function"; break;
-    case t2crt::TY_Object: out << "object"; break;
-  }
-  return out;
-}
-
 const t2crt::JS_Val undefined = { 0, t2crt::TY_Undef, false };
 
 #endif
---
+++ src/MapleFE/ast2cpp/runtime/src/ts2cpp.cpp
@@ -0,0 +1,19 @@
+#include "ts2cpp.h"
+
+std::ostream& operator<< (std::ostream& out, const t2crt::JS_Val& v) {
+  switch(v.type) {
+    case t2crt::TY_None: out << "None"; break;
+    case t2crt::TY_Undef: out << "undefined"; break;
+    case t2crt::TY_Null: out << "null"; break;
+    case t2crt::TY_Bool: out << v.x.val_bool; break;
+    case t2crt::TY_Long: out << v.x.val_long; break;
+    case t2crt::TY_Double: out << v.x.val_double; break;
+    case t2crt::TY_BigInt: out << "bigint"; break;
+    case t2crt::TY_String: out << "string"; break;
+    case t2crt::TY_Symbol: out << "symbol"; break;
+    case t2crt::TY_Function: out << "function"; break;
+    case t2crt::TY_Object: out << "object"; break;
+  }
+  return out;
+}
+
