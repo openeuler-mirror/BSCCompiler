84860c7c09e8d17fd1487d4b3183333534ab3e7d
binaryfz <fuzhou@huawei.com>
2021-07-09 10:44:47 +0800

    [mplfe]Internal commit msg:     [maplec] OpenArk_test

---
+++ src/mplfe/test/jbc_input/jbc_class_const_test.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) [2020-2021] Huawei Technologies Co.,Ltd.All rights reserved.
+ *
+ * OpenArkCompiler is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *     http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "jbc_class_const.h"
+
+namespace maple {
+namespace jbc {
+TEST(JBCClassConst, GetTagName) {
+  EXPECT_EQ(JBCConstTagName::GetTagName(kConstUTF8), "ConstUTF8");
+}
+}  // namespace jbc
+}  // namespace maple
