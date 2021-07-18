1da478d9f381465d2db2c2f6247d48c1ad6d6f20
Yan Zhang <yzhang@futurewei.com>
2021-07-18 15:28:10 -0700

    Add a test case for missing semicolon

---
+++ src/MapleFE/test/typescript/unit_tests/semicolon-missing4.ts
@@ -0,0 +1,11 @@
+class Klass {
+  func: (n : number) => number;
+}
+var obj : Klass;
+obj.func = function(n : number): number {
+  return n;
+}
+
+export function show() {
+  console.log(obj.func(123));
+}
