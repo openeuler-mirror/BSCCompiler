b1a55413b2537b67eee2e389d103361ed5f9b0cf
linma <lma1@futurewei.com>
2021-07-21 22:24:47 -0700

    add loop vectorization phase

--- src/mapleall/maple_me/include/lfo_pre_emit.h
+++ src/mapleall/maple_me/include/lfo_pre_emit.h
@@ -15,6 +15,7 @@
 
 #ifndef MAPLE_ME_INCLUDE_LFO_PRE_EMIT_H
 #define MAPLE_ME_INCLUDE_LFO_PRE_EMIT_H
+#include "mir_nodes.h"
 #include "me_irmap.h"
 #include "me_phase.h"
 
@@ -28,7 +29,7 @@ class LfoPreEmitter : public AnalysisResult {
   MapleAllocator *codeMPAlloc;
   MemPool *lfoMP;
   MapleAllocator lfoMPAlloc;
-  MapleMap<uint32_t, LfoPart*> lfoStmtParts; // map lfoinfo for StmtNode, key is stmtID
+  MapleMap<uint32_t, LfoPart*>  lfoStmtParts; // map lfoinfo for StmtNode, key is stmtID
   MapleMap<BaseNode*, LfoPart*> lfoExprParts; // map lfoinfor for exprNode, key is mirnode
   MeCFG *cfg;
 
@@ -60,10 +61,10 @@ class LfoPreEmitter : public AnalysisResult {
     lfoExprParts[funcbody] = rootlfo;
   }
   void SetLfoStmtPart(uint32_t stmtID, LfoPart* lfoInfo) {
-     lfoStmtParts[stmtID] = lfoInfo;
+    lfoStmtParts[stmtID] = lfoInfo;
   }
   void SetLfoExprPart(BaseNode *expr, LfoPart* lfoInfo) {
-     lfoExprParts[expr] = lfoInfo;
+    lfoExprParts[expr] = lfoInfo;
   }
   LfoPart* GetLfoExprPart(BaseNode *node) {
     return lfoExprParts[node];
@@ -93,6 +94,11 @@ class LfoPreEmitter : public AnalysisResult {
     LfoPart *lfopart = lfoStmtParts[stmtID];
     return lfopart->mestmt;
   }
+  MIRFunction *GetMirFunction() { return mirFunc; }
+  MemPool *GetCodeMP()  { return codeMP; }
+  MapleAllocator* GetCodeMPAlloc() { return codeMPAlloc; }
+  MapleMap<uint32_t, LfoPart*> *GetLfoStmtMap() { return &lfoStmtParts; }
+  MapleMap<BaseNode*, LfoPart*> *GetLfoExprMap() { return &lfoExprParts; }
 };
 
 /* emit ir to specified file */
