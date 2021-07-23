9c1802b59ab551e5f513866683535e9d441f01bf
Yan Zhang <yzhang@futurewei.com>
2021-07-23 14:49:17 -0700

    Add a test case for constructor with a function parameter

---
+++ src/MapleFE/test/typescript/unit_tests/ctor-with-function.ts
@@ -0,0 +1,12 @@
+//const obj = new Pool<IAdditiveLightPass>(() => ({ subModel: null!, passIdx: -1, dynamicOffsets: [], lights: [] }), 16);
+class Klass {
+  func: () => any;
+  num: number;
+  constructor(f, n) {
+    this.func = f;
+    this.num = n;
+  }
+}
+const obj = new Klass(() => ({n: 1}), 16);
+console.log(obj);
+
