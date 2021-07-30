a6bdf87a749c7419fd341c92bc4f87b4aad9a08b
Yan Zhang <yzhang@futurewei.com>
2021-07-29 17:13:43 -0700

    Encode '\' for string literal

--- src/MapleFE/scripts/maplefe-autogen.py
+++ src/MapleFE/scripts/maplefe-autogen.py
@@ -508,7 +508,7 @@ static std::string EncodeLiteral(std::string str) {{
       case '\\'': enc += "\\\\'"; break;
       case '\\"': enc += "\\\\\\""; break;
       case '\\?': enc += "\\\\?"; break;
-      //case '\\\\': enc += "\\\\\\\\"; break;
+      case '\\\\': enc += "\\\\\\\\"; break;
       case '\\a': enc += "\\\\a"; break;
       case '\\b': enc += "\\\\b"; break;
       case '\\f': enc += "\\\\f"; break;
