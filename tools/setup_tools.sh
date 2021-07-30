d5dfad27d32ed8d202e49564876d23f385a681f3
binaryfz <fuzhou@huawei.com>
2021-07-30 15:04:52 +0800

    add qemu downlink for ubuntu20.04

--- tools/setup_tools.sh
+++ tools/setup_tools.sh
@@ -146,9 +146,14 @@ if [ ! -f $TOOLS/qemu/usr/bin/qemu-aarch64 ]; then
   cd $TOOLS
   echo Start wget qemu-user ...
   rm -rf qemu
-  wget http://archive.ubuntu.com/ubuntu/pool/universe/q/qemu/qemu-user_2.11+dfsg-1ubuntu7.37_amd64.deb
   mkdir -p qemu
-  dpkg-deb -R qemu-user_2.11+dfsg-1ubuntu7.37_amd64.deb qemu
+  if [ "$OLD_OS" == "1" ];then
+    wget http://archive.ubuntu.com/ubuntu/pool/universe/q/qemu/qemu-user_2.11+dfsg-1ubuntu7.37_amd64.deb
+    dpkg-deb -R qemu-user_2.11+dfsg-1ubuntu7.37_amd64.deb qemu
+  else
+    wget http://archive.ubuntu.com/ubuntu/pool/universe/q/qemu/qemu-user_4.2-3ubuntu6.17_amd64.deb
+    dpkg-deb -R qemu-user_4.2-3ubuntu6.17_amd64.deb
+  fi
   echo Installed qemu-aarch64
 fi
 
