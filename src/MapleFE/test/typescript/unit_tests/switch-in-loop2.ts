764e8970648aadfab38fe4196fd9086c134b6608
Yan Zhang <yzhang@futurewei.com>
2021-08-02 12:02:07 -0700

    Add a test case for switch stmt in a loop

---
+++ src/MapleFE/test/typescript/unit_tests/switch-in-loop2.ts
@@ -0,0 +1,15 @@
+function func() {
+  var obj = {};
+  for (var i: number = 1; i < 4; ++i) {
+    switch (i) {
+      case 2:
+        break;
+      default:
+        obj[i] = "OK";
+      break;
+    }
+  }
+  return obj;
+}
+
+console.log(func());
