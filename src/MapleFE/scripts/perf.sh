c9df9b30ea36840570ea01a36938b8d21dbe1198
Yan Zhang <yzhang@futurewei.com>
2021-08-16 11:03:56 -0700

    Add comments to perf.sh

--- src/MapleFE/scripts/perf.sh
+++ src/MapleFE/scripts/perf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is to measure the runtime performance of ts2ast and other executables
+# If perf is not installed yet, please install the package linux-tools-common with all dependencies
 TS2AST=$(dirname $0)/../output/typescript/bin/ts2ast
 CMD="sudo perf record -e cpu-cycles,cache-misses --call-graph fp -F 10000 -o perf.data"
 if [ $# -eq 0 ]; then
