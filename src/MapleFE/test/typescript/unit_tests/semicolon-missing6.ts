0346310a7b0f761a425607a7c62cc0169cc9adbb
Yan Zhang <yzhang@futurewei.com>
2021-08-31 11:38:41 -0700

    Update semicolon-missing6.ts with a case for RegExp

--- src/MapleFE/test/typescript/unit_tests/semicolon-missing6.ts
+++ src/MapleFE/test/typescript/unit_tests/semicolon-missing6.ts
@@ -1,2 +1,4 @@
-export function func(s: string): string | null
-export function func2(s: string): string | null
+export declare function func(s: string): string | null
+export declare function func2(s: string): string | null
+export declare const re1: RegExp
+export declare const re2: RegExp
