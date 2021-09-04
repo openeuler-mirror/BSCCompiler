4be0ae4c5b513c92e9a7f3845c05a119f7fc50fb
Yan Zhang <yzhang@futurewei.com>
2021-09-04 16:59:18 -0700

    Remove unnecessary a semicolon and a new-line for DeclareNode

--- src/MapleFE/ast2cpp/src/emitter.cpp
+++ src/MapleFE/ast2cpp/src/emitter.cpp
@@ -386,8 +386,8 @@ std::string Emitter::EmitDeclareNode(DeclareNode *node) {
     if (s.substr(0, 9) == "function ")
       Replace(s, ") => ", ") : ");
     else if (n->GetKind() == NK_Module) {
-      s = "module "s + static_cast<ModuleNode *>(n)->GetFileName()
-        + " {\n"s + s + ";\n}\n"s;
+      s = "module \""s + static_cast<ModuleNode *>(n)->GetFileName()
+        + "\" {\n"s + s + "}\n"s;
     }
     str += "declare "s + s;
   }
