cbcb844956c3d351d7c713daa4bdfc7bebf2f2ad
Fred Chow <fredchow@gitee.com>
2021-09-12 08:39:10 -0700

    Implemented reassociation such that if a base pointer is never incremented, any constant offset is added to it directly
    Fixed the regression caused by previous change to FoldTypeCvt().

--- src/mapleall/maple_me/src/irmap.cpp
+++ src/mapleall/maple_me/src/irmap.cpp
@@ -222,37 +222,70 @@ IvarMeExpr *IRMap::BuildLHSIvar(MeExpr &baseAddr, PrimType primType, const TyIdx
   return meDef;
 }
 
+static MeExpr *GetTerminalBase(MeExpr *baseExpr) {
+  if (baseExpr->GetMeOp() == kMeOpOp && 
+      IsPrimitiveInteger(baseExpr->GetPrimType()) &&
+      (baseExpr->GetOp() == OP_add || baseExpr->GetOp() == OP_sub || baseExpr->GetOp() == OP_cvt)) {
+      return GetTerminalBase(static_cast<OpMeExpr *>(baseExpr)->GetOpnd(0));
+    }
+  return baseExpr;
+}
+
 MeExpr* IRMap::SimplifyIvarWithConstOffset(IvarMeExpr *ivar, bool lhsIvar) {
+  if (ivar->simplifiedWithConstOffset) {
+    return nullptr;
+  }
   auto *base = ivar->GetBase();
   if (base->GetOp() == OP_add || base->GetOp() == OP_sub) {
-    auto offsetNode = base->GetOpnd(1);
+    MeExpr *offsetNode = base->GetOpnd(1);
     if (offsetNode->GetOp() == OP_constval) {
-      // get offset value
-      auto *mirConst = static_cast<ConstMeExpr*>(offsetNode)->GetConstVal();
-      CHECK_FATAL(mirConst->GetKind() == kConstInt, "must be integer const");
-      auto offsetInByte = static_cast<MIRIntConst*>(mirConst)->GetValue();
-      OffsetType offset(ivar->GetOffset());
-      offset += (base->GetOp() == OP_add ? offsetInByte : -offsetInByte);
-      if (offset.IsInvalid()) {
-        return nullptr;
+      ScalarMeExpr *ptrVar = dynamic_cast<ScalarMeExpr *>(GetTerminalBase(base));
+      MeExpr *newBase = nullptr;
+      Opcode op = OP_iread;
+      int32 offsetVal = 0;
+      if ((ptrVar == nullptr || ptrVar->GetOst()->isPtrWithIncDec)) {
+        // get offset value
+        auto *mirConst = static_cast<ConstMeExpr*>(offsetNode)->GetConstVal();
+        CHECK_FATAL(mirConst->GetKind() == kConstInt, "must be integer const");
+        auto offsetInByte = static_cast<MIRIntConst*>(mirConst)->GetValue();
+        OffsetType offset(ivar->GetOffset());
+        offset += (base->GetOp() == OP_add ? offsetInByte : -offsetInByte);
+        if (offset.IsInvalid()) {
+          return nullptr;
+        }
+        if (offset.val != 0) {
+          op = OP_ireadoff;
+        }
+        newBase = base->GetOpnd(0);
+        offsetVal = offset.val;
+      } else {
+        // reassociate the base expression such that the constant is added directly to ptrVar
+        MeExpr *newAddSub = CreateMeExprBinary(base->GetOp(), ptrVar->GetPrimType(), *ptrVar, *offsetNode);
+        if (ptrVar == base->GetOpnd(0)) {
+          newBase = newAddSub;
+        } else {
+          OpMeExpr newMeExpr(*static_cast<OpMeExpr *>(base->GetOpnd(0)), kInvalidExprID);
+          newBase = ReplaceMeExprExpr(*base->GetOpnd(0), newMeExpr, 1, *ptrVar, *newAddSub);
+        }
       }
-
-      Opcode op = (offset.val == 0) ? OP_iread : OP_ireadoff;
       if (lhsIvar) {
         auto *meDef = New<IvarMeExpr>(exprID++, ivar->GetPrimType(), ivar->GetTyIdx(), ivar->GetFieldID(), op);
-        meDef->SetBase(base->GetOpnd(0));
-        meDef->SetOffset(offset.val);
+        meDef->SetBase(newBase);
+        meDef->SetOffset(offsetVal);
         meDef->SetMuVal(ivar->GetMu());
         meDef->SetVolatileFromBaseSymbol(ivar->GetVolatileFromBaseSymbol());
         PutToBucket(meDef->GetHashIndex() % mapHashLength, *meDef);
+        meDef->simplifiedWithConstOffset = true;
         return meDef;
       } else {
         IvarMeExpr newIvar(kInvalidExprID, ivar->GetPrimType(), ivar->GetTyIdx(), ivar->GetFieldID(), op);
-        newIvar.SetBase(base->GetOpnd(0));
-        newIvar.SetOffset(offset.val);
+        newIvar.SetBase(newBase);
+        newIvar.SetOffset(offsetVal);
         newIvar.SetMuVal(ivar->GetMu());
         newIvar.SetVolatileFromBaseSymbol(ivar->GetVolatileFromBaseSymbol());
-        return HashMeExpr(newIvar);
+        IvarMeExpr *formedIvar = static_cast<IvarMeExpr *>(HashMeExpr(newIvar));
+        formedIvar->simplifiedWithConstOffset = true;
+        return formedIvar;
       }
     }
   }
