2162c9b9b70cb45d5dd1789a83345e9236c37d9b
eching <edward.k.ching@gmail.com>
2021-10-02 00:29:51 -0700

    Extend instanceof testcase.

--- src/MapleFE/test/typescript/unit_tests/instanceof-1.ts
+++ src/MapleFE/test/typescript/unit_tests/instanceof-1.ts
@@ -1,11 +1,13 @@
 class Foo {}
 
 class Bar {
-  foo: Function = Foo;
+  foo: Function;
 }
 
 var foo:Foo = new Foo();
 var bar:Bar = new Bar();
+bar.foo = Foo;
 console.log(foo instanceof Bar);
 console.log(foo instanceof bar.foo);
+console.log(bar.foo instanceof Bar);
 
