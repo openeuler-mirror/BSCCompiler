8e5ffcccb1740675e26e8d354ec2a51e36ef772e
Yan Zhang <yzhang@futurewei.com>
2021-10-14 19:41:13 -0700

    Revert "Rename the imported var 'x' of current module to 'xx' to avoid name conflict"
    This reverts commit dc6c233fd34e3a4def50fbd272f4ac5e6c3975a5.

--- src/MapleFE/test/typescript/unit_tests/re-export.ts
+++ src/MapleFE/test/typescript/unit_tests/re-export.ts
@@ -1,4 +1,4 @@
 export * from "./M";
 export { default as x } from "./M";
-import { default as xx } from "./M";
-export { xx as default };
+import { default as x } from "./M";
+export { x as default };
