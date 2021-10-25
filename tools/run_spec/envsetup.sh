7734bab696981a6a287c026a49ce52f8f9eb4999
sunzibo <sunzibo@huawei.com>
2021-10-25 12:54:35 +0000

    remove unreasonable env config in tools/run_spec/envsetup.sh

--- tools/run_spec/envsetup.sh
+++ tools/run_spec/envsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$MAPLE_ROOT/tools/gcc-linaro-7.5.0/aarch64-linux-gnu/libc/lib
+# export LD_LIBRARY_PATH=$MAPLE_ROOT/tools/gcc-linaro-7.5.0/aarch64-linux-gnu/libc/lib
 # spec
 export SPEC=$HOME/spec2017
 echo SPEC=$SPEC
