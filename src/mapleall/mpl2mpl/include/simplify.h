c4559f28ce31371371a78d37bd5d24ecbc52d4d1
binaryfz <fuzhou@huawei.com>
2021-10-27 17:47:21 +0800

    [mapleall]Internal commit msg:     [mapleall] Rename func name in simplify

--- src/mapleall/mpl2mpl/include/simplify.h
+++ src/mapleall/mpl2mpl/include/simplify.h
@@ -14,10 +14,79 @@
  */
 #ifndef MPL2MPL_INCLUDE_SIMPLIFY_H
 #define MPL2MPL_INCLUDE_SIMPLIFY_H
-#include "module_phase.h"
 #include "phase_impl.h"
 #include "factory.h"
+#include "maple_phase_manager.h"
 namespace maple {
+// MemEntry models a memory entry with high level type information.
+struct MemEntry {
+  enum MemEntryKind {
+    kMemEntryUnknown,
+    kMemEntryPrimitive,
+    kMemEntryStruct,
+    kMemEntryArray
+  };
+
+  static bool ComputeMemEntry(BaseNode &expr, MIRFunction &func, MemEntry &memEntry, bool isLowLevel);
+  MemEntry() = default;
+  MemEntry(BaseNode *addrExpr, MIRType *memType) : addrExpr(addrExpr), memType(memType) {}
+
+  MemEntryKind GetKind() const {
+    if (memType == nullptr) {
+      return kMemEntryUnknown;
+    }
+    auto typeKind = memType->GetKind();
+    if (typeKind == kTypeScalar || typeKind == kTypePointer) {
+      return kMemEntryPrimitive;
+    } else if (typeKind == kTypeArray) {
+      return kMemEntryArray;
+    } else if (memType->IsStructType()) {
+      return kMemEntryStruct;
+    }
+    return kMemEntryUnknown;
+  }
+
+  BaseNode *BuildAsRhsExpr(MIRFunction &func) const;
+  bool ExpandMemset(int64 byte, int64 size, MIRFunction &func,
+                    CallNode &callStmt, BlockNode &block, bool isLowLevel, bool debug) const;
+  bool ExpandMemcpy(const MemEntry &srcMem, int64 copySize, MIRFunction &func,
+                    CallNode &callStmt, BlockNode &block, bool isLowLevel, bool debug) const;
+  static StmtNode *GenMemopRetAssign(CallNode &callStmt, MIRFunction &func, bool isLowLevel);
+
+  BaseNode *addrExpr = nullptr;   // memory address
+  MIRType *memType = nullptr;     // memory type, this may be nullptr for low level memory entry
+};
+
+enum MemOpKind {
+  MEM_OP_unknown,
+  MEM_OP_memset,
+  MEM_OP_memcpy
+};
+
+// For simplifying memory operation, either memset or memcpy/memmove.
+class SimplifyMemOp {
+ public:
+  static MemOpKind ComputeMemOpKind(StmtNode &stmt);
+  SimplifyMemOp() = default;
+  virtual ~SimplifyMemOp() = default;
+  explicit SimplifyMemOp(MIRFunction *func, bool debug = false) : func(func), debug(debug) {}
+  void SetFunction(MIRFunction *f) {
+    func = f;
+  }
+  void SetDebug(bool dbg) {
+    debug = dbg;
+  }
+
+  bool AutoSimplify(StmtNode &stmt, BlockNode &block, bool isLowLevel) const;
+  bool SimplifyMemset(StmtNode &stmt, BlockNode &block, bool isLowLevel) const;
+  bool SimplifyMemcpy(StmtNode &stmt, BlockNode &block, bool isLowLevel) const;
+ private:
+  static const uint32 thresholdMemsetExpand;
+  static const uint32 thresholdMemcpyExpand;
+  MIRFunction *func = nullptr;
+  bool debug = false;
+};
+
 class Simplify : public FuncOptimizeImpl {
  public:
   Simplify(MIRModule &mod, KlassHierarchy *kh, bool dump) : FuncOptimizeImpl(mod, kh, dump), mirMod(mod) {
@@ -35,28 +104,16 @@ class Simplify : public FuncOptimizeImpl {
 
  private:
   MIRModule &mirMod;
+  SimplifyMemOp simplifyMemOp;
   bool IsMathSqrt(const std::string funcName);
   bool IsMathAbs(const std::string funcName);
   bool IsMathMin(const std::string funcName);
   bool IsMathMax(const std::string funcName);
   bool SimplifyMathMethod(const StmtNode &stmt, BlockNode &block);
-  void SimplifyCallAssigned(const StmtNode &stmt, BlockNode &block);
-  StmtNode *SplitAggCopy(StmtNode *stmt, BlockNode *block, MIRFunction *func);
+  void SimplifyCallAssigned(StmtNode &stmt, BlockNode &block);
+  StmtNode *SimplifyToSelect(MIRFunction *func, IfStmtNode *ifNode, BlockNode *block);
 };
-class DoSimplify : public ModulePhase {
- public:
-  explicit DoSimplify(ModulePhaseID id) : ModulePhase(id) {}
 
-  std::string PhaseName() const override {
-    return "simplify";
-  }
-
-  AnalysisResult *Run(MIRModule *mod, ModuleResultMgr *mrm) override {
-    OPT_TEMPLATE(Simplify);
-    return nullptr;
-  }
-
-  ~DoSimplify() = default;
-};
+MAPLE_MODULE_PHASE_DECLARE(M2MSimplify)
 }  // namespace maple
 #endif  // MPL2MPL_INCLUDE_SIMPLIFY_H
