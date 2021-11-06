823f40986c5ee2f5fb4977e8291d2ec1429ab5e1
binaryfz <fuzhou@huawei.com>
2021-11-06 12:58:54 +0800

    [mapleall]Internal commit msg:     [me]: remove whitelist in aliasclass.

--- src/mapleall/maple_ipa/src/module_phase_manager.cpp
+++ src/mapleall/maple_ipa/src/module_phase_manager.cpp
@@ -13,109 +13,112 @@
  * See the Mulan PSL v2 for more details.
  */
 #include "module_phase_manager.h"
-#include "class_hierarchy.h"
-#include "class_init.h"
-#include "option.h"
-#include "bin_mpl_export.h"
-#include "mpl_timer.h"
-#include "clone.h"
-#include "call_graph.h"
-#include "verification.h"
-#include "verify_mark.h"
-#include "inline.h"
-#include "method_replace.h"
-#if MIR_JAVA
-#include "native_stub_func.h"
-#include "vtable_analysis.h"
-#include "reflection_analysis.h"
-#include "annotation_analysis.h"
-#include "vtable_impl.h"
-#include "java_intrn_lowering.h"
-#include "simplify.h"
-#include "java_eh_lower.h"
-#include "muid_replacement.h"
-#include "gen_check_cast.h"
-#include "coderelayout.h"
-#include "constantfold.h"
-#include "barrierinsertion.h"
-#include "preme.h"
-#include "scalarreplacement.h"
-#include "openProfile.h"
-#endif  // ~MIR_JAVA
+#include "me_phase_manager.h"
+#include "ipa_phase_manager.h"
+#include "ipa_side_effect.h"
 
-namespace {
-constexpr char kDotStr[] = ".";
-constexpr char kDotMplStr[] = ".mpl";
-}
+#define JAVALANG (mirModule.IsJavaModule())
+#define CLANG (mirModule.IsCModule())
 
 namespace maple {
-void ModulePhaseManager::RegisterModulePhases() {
-#define MODAPHASE(id, modphase)                                                    \
-  do {                                                                             \
-    MemPool *memPool = GetMemPool();                                               \
-    ModulePhase *phase = new (memPool->Malloc(sizeof(modphase(id)))) modphase(id); \
-    CHECK_NULL_FATAL(phase);                                                       \
-    RegisterPhase(id, *phase);                                                     \
-    arModuleMgr->AddAnalysisPhase(id, phase);                                      \
-  } while (0);
-
-#define MODTPHASE(id, modPhase)                                                    \
-  do {                                                                             \
-    MemPool *memPool = GetMemPool();                                               \
-    ModulePhase *phase = new (memPool->Malloc(sizeof(modPhase(id)))) modPhase(id); \
-    CHECK_NULL_FATAL(phase);                                                       \
-    RegisterPhase(id, *phase);                                                     \
-  } while (0);
-#include "module_phases.def"
-#undef MODAPHASE
-#undef MODTPHASE
-}
-
-void ModulePhaseManager::AddModulePhases(const std::vector<std::string> &phases) {
-  for (std::string const &phase : phases) {
-    AddPhase(phase);
+void DumpModule(const MIRModule &mod, const std::string phaseName, bool isBefore) {
+  MIRFunction *func = nullptr;
+  if (Options::DumpFunc()) {
+    GStrIdx gStrIdxOfFunc = GlobalTables::GetStrTable().GetStrIdxFromName(Options::dumpFunc);
+    func = GlobalTables::GetGsymTable().GetSymbolFromStrIdx(gStrIdxOfFunc)->GetFunction();
+  }
+  bool dumpPhase = Options::DumpPhase(phaseName);
+  if (Options::dumpBefore && dumpPhase && isBefore) {
+    LogInfo::MapleLogger() << ">>>>> Dump before " << phaseName << " <<<<<\n";
+    if (func != nullptr) {
+      func->Dump(false);
+    } else {
+      mod.Dump();
+    }
+    LogInfo::MapleLogger() << ">>>>> Dump before End <<<<<\n";
+    return;
+  }
+  if ((Options::dumpAfter || dumpPhase) && !isBefore) {
+    LogInfo::MapleLogger() << ">>>>> Dump after " << phaseName << " <<<<<\n";
+    if (func != nullptr) {
+      func->Dump(false);
+    } else {
+      mod.Dump();
+    }
+    LogInfo::MapleLogger() << ">>>>> Dump after End <<<<<\n\n";
   }
 }
 
-void ModulePhaseManager::Run() {
-  int phaseIndex = 0;
-  for (auto it = PhaseSequenceBegin(); it != PhaseSequenceEnd(); ++it, ++phaseIndex) {
-    PhaseID id = GetPhaseId(it);
-    auto *p = static_cast<ModulePhase*>(GetPhase(id));
-    MIR_ASSERT(p);
-    // if we need to skip after certain pass
-    if (Options::skipFrom.compare(p->PhaseName()) == 0) {
-      break;
+void MEBETopLevelManager::InitFuncDescWithWhiteList(maple::MIRModule &mod) {
+  for (auto &pair : SideEffect::GetWhiteList()) {
+    MIRFunction *func = mod.GetMIRBuilder()->GetFunctionFromName(pair.first);
+    if (func == nullptr) {
+      continue;
     }
-    if (!Options::quiet) {
-      LogInfo::MapleLogger() << "---Run Module Phase [ " << p->PhaseName() << " ]---\n";
+    const FuncDesc &desc = pair.second;
+    func->SetFuncDesc(desc);
+    if (desc.IsPure() || desc.IsConst()) {
+      func->SetAttr(FUNCATTR_pure);
+      func->SetAttr(FUNCATTR_nodefargeffect);
+      func->SetAttr(FUNCATTR_nodefeffect);
+      func->SetAttr(FUNCATTR_nothrow_exception);
     }
-    MPLTimer timer;
-    if (timePhases) {
-      timer.Start();
-    }
-    auto *analysisRes = p->Run(&mirModule, arModuleMgr);
-    if (timePhases) {
-      timer.Stop();
-      phaseTimers[phaseIndex] += timer.ElapsedMicroseconds();
-    }
-    if (Options::skipAfter.compare(p->PhaseName()) == 0) {
-      break;
-    }
-    p->ClearMemPoolsExcept(analysisRes == nullptr ? nullptr : analysisRes->GetMempool());
   }
 }
 
-void ModulePhaseManager::Emit(const std::string &passName) {
-  std::string outFileName;
-  std::string moduleFileName = mirModule.GetFileName();
-  std::string::size_type lastDot = moduleFileName.find_last_of(kDotStr);
-  if (lastDot == std::string::npos) {
-    outFileName = moduleFileName + kDotStr;
-  } else {
-    outFileName = moduleFileName.substr(0, lastDot) + kDotStr;
+void MEBETopLevelManager::Run(maple::MIRModule &mod) {
+  InitFuncDescWithWhiteList(mod);
+  auto admMempool = AllocateMemPoolInPhaseManager("MEBETopLevelManager's Analysis Data Manager mempool");
+  auto *serialADM = GetManagerMemPool()->New<AnalysisDataManager>(*(admMempool.get()));
+  bool changed = false;
+  for (size_t i = 0; i < phasesSequence.size(); ++i) {
+    const MaplePhaseInfo *curPhase = MaplePhaseRegister::GetMaplePhaseRegister()->GetPhaseByID(phasesSequence[i]);
+    if (!IsQuiet()) {
+      LogInfo::MapleLogger() << "<<<Run Module " << (curPhase->IsAnalysis() ? "analysis" : "transform")
+                             << " Phase [ " << curPhase->PhaseName()  << " ]>>>\n";
+    }
+    DumpModule(mod, curPhase->PhaseName(), true);
+    if (curPhase->IsAnalysis()) {
+      changed |= RunAnalysisPhase<MapleModulePhase, MIRModule>(*curPhase, *serialADM, mod);
+    } else {
+      changed |= RunTransformPhase<MapleModulePhase, MIRModule>(*curPhase, *serialADM, mod);
+    }
+    DumpModule(mod, curPhase->PhaseName(), false);
   }
-  outFileName = outFileName.append(passName).append(kDotMplStr);
-  mirModule.DumpToFile(outFileName);
 }
+
+void MEBETopLevelManager::DoPhasesPopulate(const maple::MIRModule &mirModule) {
+#define MODULE_PHASE
+#include "phases.def"
+#undef MODULE_PHASE
+}
+
+MAPLE_TRANSFORM_PHASE_REGISTER(MEBETopLevelManager, TopLevelManager)
+
+MAPLE_ANALYSIS_PHASE_REGISTER(IpaSccPM, IpaSccPM)
+MAPLE_ANALYSIS_PHASE_REGISTER(M2MClone, clone)
+MAPLE_ANALYSIS_PHASE_REGISTER(M2MCallGraph, callgraph)
+MAPLE_ANALYSIS_PHASE_REGISTER(M2MKlassHierarchy, classhierarchy)
+MAPLE_ANALYSIS_PHASE_REGISTER(M2MAnnotationAnalysis, annotationanalysis)
+
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MInline, inline)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MIPODevirtualize, ipodevirtulize)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MMethodReplace, methodreplace)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MScalarReplacement, ScalarReplacement)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MOpenprofile, openprofile)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MPreme, preme)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MPreCheckCast, precheckcast)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MCheckCastGeneration, gencheckcast)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MMuidReplacement, MUIDReplacement)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MGenerateNativeStubFunc, GenNativeStubFunc)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MJavaEHLowerer, javaehlower)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MCodeReLayout, CodeReLayout)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MClassInit, clinit)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MJavaIntrnLowering, javaintrnlowering)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MSimplify, simplify)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MUpdateMplt, updatemplt)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MReflectionAnalysis, reflectionanalysis)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MVtableImpl, VtableImpl)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MConstantFold, ConstantFold)
+MAPLE_TRANSFORM_PHASE_REGISTER(M2MVtableAnalysis, vtableanalysis)
 }  // namespace maple
