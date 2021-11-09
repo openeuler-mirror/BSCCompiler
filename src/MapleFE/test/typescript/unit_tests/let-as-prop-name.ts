63fdbfd7f82698c7fce643cbaad8723815d9e643
Yan Zhang <yzhang@futurewei.com>
2021-11-09 12:46:43 -0800

    Append some code for 'var' and 'break' as property names

--- src/MapleFE/test/typescript/unit_tests/let-as-prop-name.ts
+++ src/MapleFE/test/typescript/unit_tests/let-as-prop-name.ts
@@ -8,3 +8,25 @@ export function func(arg: {let: Function | null}): void {
 
 func({let: null});
 func({let: () => { console.log("calling let()"); }});
+
+export function func1(arg: {var: Function | null}): void {
+  if (!arg.var) {
+    console.log("null");
+  } else {
+    arg.var();
+  }
+}
+
+func1({var: null});
+func1({var: () => { console.log("calling var()"); }});
+
+export function func2(arg: {break: Function | null}): void {
+  if (!arg.break) {
+    console.log("null");
+  } else {
+    arg.break();
+  }
+}
+
+func2({break: null});
+func2({break: () => { console.log("calling break()"); }});
