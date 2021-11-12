781ebedb53b28e2ecd0ca0fa849779f3f7af70ab
Yan Zhang <yzhang@futurewei.com>
2021-11-12 15:50:20 -0800

    Update identifiers11.ts to make it be compiled with the option --strict of tsc

--- src/MapleFE/test/typescript/unit_tests/identifiers11.ts
+++ src/MapleFE/test/typescript/unit_tests/identifiers11.ts
@@ -2,6 +2,6 @@ enum ET {
     type = "type",
     with = "with"
 }
-abstract class Base<U extends ET, V extends ET> { f1: U; f2: V; }
+abstract class Base<U extends ET, V extends ET> { f1: U | null = null; f2: V | null = null; }
 declare class Klass extends Base<ET.type, ET.with> {}
 
