4d1e7ac684e70c136f97f565a37b50463c013af8
Yan Zhang <yzhang@futurewei.com>
2021-11-11 20:31:27 -0800

    Add a reference link for the proposal: export as namespace for UMD module output

--- src/MapleFE/test/typescript/unit_tests/as-namespace.d.ts
+++ src/MapleFE/test/typescript/unit_tests/as-namespace.d.ts
@@ -1,3 +1,6 @@
-export var x: number;
+// Proposal: export as namespace for UMD module output
+// https://github.com/microsoft/TypeScript/issues/26532
+
 // https://www.typescriptlang.org/docs/handbook/modules.html#umd-modules
+export var x: number;
 export as namespace NS;
