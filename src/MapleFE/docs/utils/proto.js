4e2f321029e6fa9d71338e8c114098eb4131a3e6
Yan Zhang <yzhang@futurewei.com>
2021-12-14 17:48:55 -0800

    Add generator

--- src/MapleFE/docs/utils/proto.js
+++ src/MapleFE/docs/utils/proto.js
@@ -13,20 +13,30 @@ let car = new Car("A car");
 let mycar = new MyCar("My car");
 let arr = []
 
+function* generator() {}
+
 // Dump graphs with edges for prototype, __proto__ and constructor properties of each objects
-for(let g = 0; g < 2; ++g) {
-  let objs =  [  arr,   Array,   Vehicle,   Car,   MyCar,   car,   mycar,   Function,   Object,  ];
-  let names = [ "arr", "Array", "Vehicle", "Car", "MyCar", "car", "mycar", "Function", "Object", ];
-  console.log("digraph JS" + g + " {\nranksep=0.6;\nnodesep=0.6;\n" + (g == 0 ? "newrank=true;\n" : ""));
+for(let g = 0; g < 4; ++g) {
+  let objs =  [], names = [];
+  if (g < 2) {
+    objs.push(  arr,   Array,   Vehicle,   Car,   MyCar,   car,   mycar,   Function,   Object);
+    names.push("arr", "Array", "Vehicle", "Car", "MyCar", "car", "mycar", "Function", "Object");
+  } else {
+    let gen = generator.prototype.__proto__;
+    objs.push(generator,     generator(),       generator.constructor,  gen,         gen.__proto__,    Function,   Object);
+    names.push("generator", "generator_instance", "GeneratorFunction", "Generator", "GeneratorProto", "Function", "Object");
+  }
+  console.log("digraph JS" + g + " {\nranksep=0.6;\nnodesep=0.6;\n" + (g % 2 == 0 ? "newrank=true;\n" : ""));
   let num = objs.length;
   let k = num;
   // Add prototype objects and edges for them
   for(let i = 0; i < num; ++i) {
     let x = typeof objs[i].prototype;
+    console.log("// " + names[i] + ": prototype: " + x + ", __proto__:" + typeof objs[i].__proto__);
     if(x === "function" || x === "object") {
       objs[k] = objs[i].prototype;
       names[k] = names[i] + "_prototype";
-      console.log(g == 0 ? "subgraph cluster_" + names[i] + " {\nrank=same;\ncolor=white;\n"
+      console.log(g % 2 == 0 ? "subgraph cluster_" + names[i] + " {\nrank=same;\ncolor=white;\n"
         + names[i] + ";\n" + names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];\n }"
         : names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];");
       console.log(names[i] + " -> " + names[k] + " [label=\"prototype\", color=blue, fontcolor=blue];");
@@ -38,7 +48,7 @@ for(let g = 0; g < 2; ++g) {
   for(let i = 0; i < num; ++i) {
     for(let j = 0; j < num; ++j) {
       // Edges for constructor properties in the second graph only
-      if(g > 0 && objs[i].constructor === objs[j])
+      if(g % 2 == 1 && objs[i].constructor === objs[j])
         console.log(names[i] + " -> " + names[j] + " [label=\"ctor\", color=darkgreen, fontcolor=darkgreen];");
       // Edges for __proto__ properties
       if(objs[i].__proto__ === objs[j])
