8fa47e28c4474a7bc38a242b094db3a77ea03fcb
Yan Zhang <yzhang@futurewei.com>
2021-12-14 22:12:20 -0800

    Update the generated graphs based on https://262.ecma-international.org/12.0/#sec-generatorfunction-objects

--- src/MapleFE/docs/utils/proto.js
+++ src/MapleFE/docs/utils/proto.js
@@ -17,14 +17,16 @@ function* generator() {}
 
 // Dump graphs with edges for prototype, __proto__ and constructor properties of each objects
 for(let g = 0; g < 4; ++g) {
-  let objs =  [], names = [];
+  let objs =  [], names = [], gen = undefined;
   if (g < 2) {
     objs.push(  arr,   Array,   Vehicle,   Car,   MyCar,   car,   mycar,   Function,   Object);
     names.push("arr", "Array", "Vehicle", "Car", "MyCar", "car", "mycar", "Function", "Object");
   } else {
-    let gen = generator.prototype.__proto__;
-    objs.push(generator,     generator(),       generator.constructor,  gen,         gen.__proto__,    Function,   Object);
-    names.push("generator", "generator_instance", "GeneratorFunction", "Generator", "GeneratorProto", "Function", "Object");
+    gen = generator.prototype.__proto__;
+    objs.push(  generator,   generator(),          generator.constructor,  gen);
+    names.push("generator", "generator_instance", "GeneratorFunction",    "GeneratorPrototype");
+    objs.push(  gen.__proto__,       Function,   Object);
+    names.push("IteratorPrototype", "Function", "Object");
   }
   console.log("digraph JS" + g + " {\nranksep=0.6;\nnodesep=0.6;\n" + (g % 2 == 0 ? "newrank=true;\n" : ""));
   let num = objs.length;
@@ -32,13 +34,18 @@ for(let g = 0; g < 4; ++g) {
   // Add prototype objects and edges for them
   for(let i = 0; i < num; ++i) {
     let x = typeof objs[i].prototype;
-    console.log("// " + names[i] + ": prototype: " + x + ", __proto__:" + typeof objs[i].__proto__);
+    console.log("/* " + names[i] + ": prototype: " + x + ", __proto__: " + typeof objs[i].__proto__ + "\n",
+      Object.getOwnPropertyNames(objs[i]), "*/");
     if(x === "function" || x === "object") {
       objs[k] = objs[i].prototype;
-      names[k] = names[i] + "_prototype";
+      names[k] = names[i] === "GeneratorFunction" ? "Generator" : names[i] + "Prototype";
+      if(names[i] === "GeneratorFunction" && objs[k].prototype === gen) // special case for Generator
+        console.log("GeneratorPrototype -> " + names[k] + " [label=\"prototype\", color=blue, fontcolor=blue, dir=back];");
+      console.log("/* " + names[k] + ": prototype: " + typeof objs[k] + ", __proto__: " + typeof objs[k].__proto__ + "\n",
+        Object.getOwnPropertyNames(objs[k]), "*/");
       console.log(g % 2 == 0 ? "subgraph cluster_" + names[i] + " {\nrank=same;\ncolor=white;\n"
-        + names[i] + ";\n" + names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];\n }"
-        : names[k] + "[label=\"" + names[i] + ".prototype\", shape=box];");
+        + names[i] + ";\n" + names[k] + "[label=\"" + names[k] + "\", shape=box];\n }"
+        : names[k] + "[label=\"" + names[k] + "\", shape=box];");
       console.log(names[i] + " -> " + names[k] + " [label=\"prototype\", color=blue, fontcolor=blue];");
       k++;
     }
