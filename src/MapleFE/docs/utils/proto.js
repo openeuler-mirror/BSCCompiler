407ba047eec34283bcbb4a662bf1e0978dea192a
Yan Zhang <yzhang@futurewei.com>
2021-12-20 15:55:09 -0800

    Fix up every object with null as its name in a graph

--- src/MapleFE/docs/utils/proto.js
+++ src/MapleFE/docs/utils/proto.js
@@ -14,7 +14,7 @@ let mycar = new MyCar("My car");
 let arr = [1, 2, 3]
 
 function* generator() { yield 1; }
-const gen = generator.prototype.__proto__;
+const gpt = generator.prototype.__proto__;
 
 function makeClosure(a) {
   return function (b) {
@@ -26,23 +26,27 @@ const closure = makeClosure(1);
 // All data for generating graphs
 let data = [
   ["Classes",   ["Array", "arr", "mycar", "car"]],
-  ["Generator", ["generator", [generator(), "generator_instance"], [generator.__proto__, "Generator"],
-                 [generator.constructor, "GeneratorFunction"], [gen, "GeneratorPrototype"], [gen.__proto__, "IteratorPrototype"]]],
+  ["Generator", ["generator", [generator(), "generator_instance"], [gpt, "GeneratorPrototype"],
+                 [gpt.__proto__, "IteratorPrototype"], [gpt.constructor, "Generator"]]],
   ["Builtins",  ["Symbol", "Math", "JSON", "Promise"]],
   ["Closure",   ["makeClosure", "closure"]],
 ];
 
-// Gether all reachable objects from their prototype, __proto__ and constructor properties
-function insert(graph, ...args) {
+// Gather all reachable objects from their prototype, __proto__ and constructor properties
+function insert(g, depth, ...args) {
   for (let arg of args) {
-    let [obj, name]  = typeof arg === "string" ? [eval(arg), arg] : arg;
-    if (typeof obj !== "undefined" && obj !== null)
-      if (!graph.has(obj)) {
-        graph.set(obj, name !== null || typeof obj !== "function" ? name : obj.toString().split(" ")[1].replace(/[^a-zA-Z0-9+]/g, ""));
-        insert(graph, [obj.prototype, graph.get(obj) + "Prototype"], [obj.__proto__, null], [obj.constructor, null]);
+    let [o, name] = typeof arg === "string" ? [eval(arg), arg] : arg;
+    if (typeof o !== "undefined" && o !== null)
+      if (!g.has(o)) {
+        g.set(o, name !== null || typeof o !== "function" ? name : o.toString().split(" ")[1].replace(/[^a-zA-Z0-9+]/g, ""));
+        insert(g, ++depth, [o.prototype, g.get(o) === null ? null : g.get(o) + "Prototype"], [o.__proto__, null], [o.constructor, null]);
       } else if (name !== null)
-        graph.set(obj, name);
+        g.set(o, name);
   }
+  if (depth === 0)
+    for (let [index, entry] of Array.from(g).entries())
+      if (entry[1] === null) // Fix up every object with null as its name
+        g.set(entry[0], "Node_" + index);
 }
 
 // Dump graphs with edges for prototype, __proto__ and constructor properties of each object
@@ -50,7 +54,7 @@ let nodejs = (typeof process !== 'undefined') && (process.release.name === 'node
 for (let entry of data) {
   let [title, objs] = entry;
   let graph = new Map();
-  insert(graph, "Function", "Object", ...objs);
+  insert(graph, 0, "Function", "Object", ...objs);
   for (let ctor of ["", "_with_ctors"]) {
     console.log("digraph JS_" + title + ctor + " {\nrankdir = TB;\nranksep=0.6;\nnodesep=0.6;\n" + (ctor != "" ? "" : "newrank=true;"));
     for (let [key, value] of graph) {
@@ -62,7 +66,7 @@ for (let entry of data) {
       // Add edges for prototype properties of objects
       if (typeof key.prototype !== "undefined" && key.prototype !== null)
         console.log((ctor != "" ? "" : "subgraph cluster_" + value
-          + " {\nrank=same;\ncolor=white;\n" + value + ";\n" + graph.get(key.prototype) + "[shape=box];\n }")
+          + " {\nrank=same;\ncolor=white;\n" + value + ";\n" + graph.get(key.prototype) + "[shape=box];\n}\n")
           + value + " -> " + graph.get(key.prototype) + " [label=\"prototype\", color=blue, fontcolor=blue];");
       // Add edges for constructor properties of objects
       if (ctor != "" && key.constructor !== "undefined" && key.constructor !== null)
