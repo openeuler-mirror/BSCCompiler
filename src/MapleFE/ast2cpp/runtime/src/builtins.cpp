f288dc26dab08da60c5d6331d28db99e850ff580
eching <edward.k.ching@gmail.com>
2022-01-08 22:06:11 -0800

    Nest array constructor class inside array class, and make array constructor function a static field of array class. Implement uniform naming of array constructor functions to support all array types.

--- src/MapleFE/ast2cpp/runtime/src/builtins.cpp
+++ src/MapleFE/ast2cpp/runtime/src/builtins.cpp
@@ -6,12 +6,12 @@ Object::Ctor   Object::ctor = Object::Ctor    (&Function::ctor, Function::ctor.p
 Function::Ctor Function::ctor = Function::Ctor(&Function::ctor, Function::ctor.prototype, Object::ctor.prototype);
 Number::Ctor   Number::ctor = Number::Ctor    (&Function::ctor, Function::ctor.prototype, Object::ctor.prototype);
 
-ARRAY_CTOR_DEF(int)
-ARRAY_CTOR_DEF(long)
-ARRAY_CTOR_DEF(double)
-ARRAY_CTOR_DEF(JS_Val)
-ARRAY_CTOR_DEF(Object)
-ARRAY_CTOR_DEF(ObjectP)
+ARR_CTOR_DEF(int)
+ARR_CTOR_DEF(long)
+ARR_CTOR_DEF(double)
+ARR_CTOR_DEF(JS_Val)
+ARR_CTOR_DEF(Object)
+ARR_CTOR_DEF(Object*)
 
 } // namepsace t2crt
 
