659748468b3583e0b010a5e4e90b650610454dda
binaryfz <fuzhou@huawei.com>
2022-01-14 11:59:38 +0800

    [mapleall]Internal commit msg:     [mplcg]add new ico opt gotoBB preds only has ifBB

--- src/mapleall/maple_be/src/cg/cg_cfg.cpp
+++ src/mapleall/maple_be/src/cg/cg_cfg.cpp
@@ -564,7 +564,7 @@ bool CGCFG::IsCompareAndBranchInsn(const Insn &insn) const {
 }
 
 Insn *CGCFG::FindLastCondBrInsn(BB &bb) const {
-  if (bb.GetKind() != BB::kBBIf) {
+  if (bb.GetKind() != BB::kBBIf && bb.GetKind() != BB::kBBGoto) {
     return nullptr;
   }
   FOR_BB_INSNS_REV(insn, (&bb)) {
