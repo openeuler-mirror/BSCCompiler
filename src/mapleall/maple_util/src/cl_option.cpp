ea3a7d8fae66c9a073b4fe7004377c84dc14f297
Alex Antonov <sasha.antonov1989@gmail.com>
2022-02-14 17:08:47 +0300

    [mapleall][utils] Add commandline library in utils
    This library makes it easy to work with command line options.
    This library allows to create and register the options with
    one line of code:
    
    cl::Option<bool> opt1({"--key"}, "Description"); // bool option
    cl::Option<std::string> opt2({"--key2"}, ""); // string option
    cl::Option<int32_t> opt3({"--key3"}, ""); // int option
    
    All options are parsed with:
    cl::CommandLine::GetCommandLine()->Parse(argc, (char **)argv);
    
    Then you can use these options like this:
    if (opt1 == true) {
       //your code
    }
    std::string data = opt2;
    int32_t dig = opt3;
    
    Also it supports the following features:
    * Joined Options: cl::kValueJoined allows to register joined options,
      like -DMACRO. Example:
      cl::Option<std::string> macro({"-D"}, "", cl::kValueJoined);
    
    * Options with required and optional value:
      cl::kValueRequired/kValueOptional/kValueDisallowed allows to
      configure whether the option can be used without a value (--key),
      or value is required (--key value). Example:
    
      cl::Option<int32_t> reqVal({"--reqval"}, "", cl::kValueRequired);
      cl::Option<int32_t> optVal({"--optval"}, "", cl::kValueOptional);
      cl::Option<int32_t> woVal({"--woval"}, "", cl::kValueDisallowed);
    
    * Default Value: cl::Init allows to set default option value. If you
      do not set the option with command line, this option will use
      this default value. Example:
      cl::Option<std::string> defaultString({"--defstring"}, "", cl::Init("Default String"));
    
    * cl::DisableWith: The library allows to add special name for the options to turn off them.
      Example:
      cl::Option<bool> enable({"--enable"}, "", cl::Init(true), cl::DisableWith("--no-enable"));
    
    * These features can be set in any order. Example:
      cl::Option<int32_t> reqVal1({"--reqval1"}, "", cl::kValueRequired, cl::Init(-42));
      cl::Option<int32_t> reqVal2({"--reqval2"}, "", cl::Init(-42), cl::kValueRequired);
      In this example, the order of kValueRequired and cl::Init is different, but the library
      can handle both cases.
    
    * Categories: To register different options for the different tools
      (driver, hir2mpl, cg, etc...), the categories are implemented.
      This allows to register the options for different categories. Some
      options can be available in one category, but not be available in
      another. Example:
      cl::OptionCategory testCategory1;
      cl::OptionCategory testCategory2;
      cl::Option<bool> cat1Opt1({"--c1opt1"}, "", {&testCategory1});
      cl::Option<bool> cat12Opt({"--c12opt"}, "", {&testCategory1, &testCategory2});

---
+++ src/mapleall/maple_util/src/cl_option.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) [2022] Huawei Technologies Co.,Ltd.All rights reserved.
+ *
+ * OpenArkCompiler is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *     http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+#include "cl_option.h"
+#include "cl_parser.h"
+#include <algorithm>
+#include <iterator>
+
+using namespace cl;
+
+void OptionInterface::FinalizeInitialization(const std::vector<std::string> &optnames,
+                                             const std::string &descr,
+                                             const std::vector<OptionCategory *> &optionCategories) {
+  optDescription = descr;
+  auto &cl = CommandLine::GetCommandLine();
+
+  std::copy(optnames.begin(), optnames.end(), std::back_inserter(names));
+
+  if (optionCategories.empty()) {
+    cl.Register(optnames, *this, cl.defaultCategory);
+  } else {
+    for (auto &cat : optionCategories) {
+      cl.Register(optnames, *this, *cat);
+    }
+  }
+}
