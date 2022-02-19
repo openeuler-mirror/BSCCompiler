30beb25481e018e7b5b6169ec1de64cf406211df
Wen HU <git_huwen@yahoo.com>
2022-02-19 18:09:31 -0500

    update license year

--- src/MapleFE/ast2cpp/include/helper.h
+++ src/MapleFE/ast2cpp/include/helper.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) [2021] Futurewei Technologies, Inc. All rights reverved.
+* Copyright (C) [2021-2022] Futurewei Technologies, Inc. All rights reverved.
 *
 * OpenArkFE is licensed under the Mulan PSL v2.
 * You can use this software according to the terms and conditions of the Mulan PSL v2.
