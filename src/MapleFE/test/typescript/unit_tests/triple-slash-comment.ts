6b5b20b9423284e04977a4548b2de0f23e407ab5
Yan Zhang <yzhang@futurewei.com>
2022-03-02 10:57:06 -0800

    Add more comments starting with triple slash

--- src/MapleFE/test/typescript/unit_tests/triple-slash-comment.ts
+++ src/MapleFE/test/typescript/unit_tests/triple-slash-comment.ts
@@ -1,7 +1,10 @@
 enum ET {
   TOP = "top", /// < top string
-  BOTTOM = "bottom",
+  BOTTOM = "bottom", ///< bottom string
 }
+
+/// <param name="pn"></param>
+/// <returns></returns>
 
 let et = ET.TOP;
 console.log(et);
