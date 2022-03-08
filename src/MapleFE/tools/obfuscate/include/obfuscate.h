349c0b084a37ee43103f59f26c28672fb19cfcc2
Wen HU <git_huwen@yahoo.com>
2022-03-08 10:53:03 -0500

    add tools/obfuscate

---
+++ src/MapleFE/tools/obfuscate/include/obfuscate.h
@@ -0,0 +1,52 @@
+/*
+* Copyright (C) [2022] Futurewei Technologies, Inc. All rights reverved.
+*
+* OpenArkFE is licensed under the Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*
+*  http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+* FIT FOR A PARTICULAR PURPOSE.
+* See the Mulan PSL v2 for more details.
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//                This is the interface to translate AST to C++
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __OBFUSCATE_HEADER__
+#define __OBFUSCATE_HEADER__
+
+#include "astopt.h"
+#include "ast_handler.h"
+#include "ast_module.h"
+
+namespace maplefe {
+
+class Obfuscate : public AstOpt {
+private:
+  AST_Handler *mASTHandler;
+  unsigned     mFlags;
+  unsigned     mIndexImported;
+
+public:
+  explicit Obfuscate(AST_Handler *h, unsigned flags) :
+    AstOpt(h, flags),
+    mASTHandler(h),
+    mFlags(flags),
+    mIndexImported(0) {}
+  ~Obfuscate() = default;
+
+  void EmitTS();
+  bool LoadImportedModules();
+
+  // return 0 if successful
+  // return non-zero if failed
+  int ProcessAST();
+};
+
+}
+#endif
