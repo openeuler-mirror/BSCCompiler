d066b7fc5495e97247db784d120b73f9d3d2ac59
William Chen <william.chen@futurewei.com>
2022-06-08 15:52:44 -0700

    Fred's Fixed determination logic of when there is fake parameter for returning struct, where x8 is also used to pass the fake parameter

--- src/mapleall/maple_be/src/cg/aarch64/aarch64_args.cpp
+++ src/mapleall/maple_be/src/cg/aarch64/aarch64_args.cpp
@@ -38,7 +38,7 @@ void AArch64MoveRegArgs::CollectRegisterArgs(std::map<uint32, AArch64reg> &argsL
   uint32 start = 0;
   if (numFormal) {
     MIRFunction *func = const_cast<MIRFunction *>(aarchCGFunc->GetBecommon().GetMIRModule().CurFunction());
-    if (func->IsFirstArgReturn()) {
+    if (func->IsReturnStruct() && func->IsFirstArgReturn()) {
       TyIdx tyIdx = func->GetFuncRetStructTyIdx();
       if (aarchCGFunc->GetBecommon().GetTypeSize(tyIdx) <= k16ByteSize) {
         start = 1;
@@ -436,7 +436,7 @@ void AArch64MoveRegArgs::MoveVRegisterArgs() {
   uint32 start = 0;
   if (formalCount) {
     MIRFunction *func = const_cast<MIRFunction*>(aarchCGFunc->GetBecommon().GetMIRModule().CurFunction());
-    if (func->IsFirstArgReturn()) {
+    if (func->IsReturnStruct() && func->IsFirstArgReturn()) {
       TyIdx tyIdx = func->GetFuncRetStructTyIdx();
       if (aarchCGFunc->GetBecommon().GetTypeSize(tyIdx) <= k16BitSize) {
         start = 1;
