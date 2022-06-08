c412e0b848b0ca85b482bb2ec864f86443347984
William Chen <william.chen@futurewei.com>
2022-06-08 15:53:00 -0700

    Remove unneeded fp def. Turn on tailcall. no unneeded pro/epilog.

--- src/mapleall/maple_be/src/cg/aarch64/aarch64_color_ra.cpp
+++ src/mapleall/maple_be/src/cg/aarch64/aarch64_color_ra.cpp
@@ -3609,6 +3609,9 @@ RegOperand *GraphColorRegAllocator::GetReplaceOpnd(Insn &insn, const Operand &op
   auto &regOpnd = static_cast<const RegOperand&>(opnd);
 
   uint32 vregNO = regOpnd.GetRegisterNumber();
+  if (vregNO == RFP) {
+    seenFP = true;
+  }
   RegType regType = regOpnd.GetRegisterType();
   if (vregNO < kAllRegNum) {
     return nullptr;
@@ -4976,6 +4979,9 @@ bool GraphColorRegAllocator::AllocateRegisters() {
 
   MarkCalleeSaveRegs();
 
+  if (seenFP == false) {
+    cgFunc->UnsetSeenFP();
+  }
   if (GCRA_DUMP) {
     cgFunc->DumpCGIR();
   }
