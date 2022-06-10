42f7e1be29dd5b692b0bddb3c8aecfbf79a60ead
Wen HU <git_huwen@yahoo.com>
2022-06-10 12:14:10 -0400

    remove some test cases

--- src/MapleFE/test/java/openjdk/Double-2.java
+++
@@ -1,5 +0,0 @@
-public final class Double extends Number implements Comparable<Double> {
-    public boolean equals(Object obj) {
-        return doubleToLongBits(((Double)obj).value) == doubleToLongBits(value);
-    }
-}
