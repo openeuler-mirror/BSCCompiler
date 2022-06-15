c6fbfd66dd5506fd16492376ed55535a71a0bc77
binaryfz <fuzhou@huawei.com>
2022-06-15 10:04:23 +0800

    [mapleall]Internal commit msg:     !1140 debug: setup DIE of DW_TAG_union_type at setupDBGInfo     !1142 lfo vectorization: fix a type issue caused by compare node     !1141 remove adjacent occurrence of sext/zext of the same width

--- src/mapleall/maple_be/src/cg/emit.cpp
+++ src/mapleall/maple_be/src/cg/emit.cpp
@@ -381,11 +381,7 @@ void Emitter::EmitAsmLabel(const MIRSymbol &mirSymbol, AsmLabel label) {
       } else {
         size = std::to_string(Globals::GetInstance()->GetBECommon()->GetTypeSize(mirType->GetTypeIndex()));
       }
-      Emit(asmInfo->GetComm());
-      Emit(symName);
-      Emit(", ");
-      Emit(size);
-      Emit(", ");
+      Emit(asmInfo->GetComm()).Emit(symName).Emit(", ").Emit(size).Emit(", ");
 #if PECOFF
 #if TARGARM || TARGAARCH64 || TARGARK || TARGRISCV64
       std::string align = std::to_string(
@@ -404,11 +400,7 @@ void Emitter::EmitAsmLabel(const MIRSymbol &mirSymbol, AsmLabel label) {
       } else if (((kind == kTypeStruct) || (kind == kTypeClass) || (kind == kTypeArray) || (kind == kTypeUnion)) &&
                  ((storage == kScGlobal) || (storage == kScPstatic) || (storage == kScFstatic))) {
         int32 align = Globals::GetInstance()->GetBECommon()->GetTypeAlign(mirType->GetTypeIndex());
-        if (kSizeOfPtr < align) {
-          Emit(std::to_string(align));
-        } else {
-          Emit(std::to_string(k8ByteSize));
-        }
+        Emit(std::to_string(align > kSizeOfPtr ? align : k8BitSize));
       } else {
         Emit(std::to_string(Globals::GetInstance()->GetBECommon()->GetTypeAlign(mirType->GetTypeIndex())));
       }
@@ -755,6 +747,7 @@ void Emitter::EmitScalarConstant(MIRConst &mirConst, bool newLine, bool flag32, 
       StIdx stIdx = symAddr.GetSymbolIndex();
       MIRSymbol *symAddrSym = stIdx.IsGlobal() ? GlobalTables::GetGsymTable().GetSymbolFromStidx(stIdx.Idx())
           : CG::GetCurCGFunc()->GetMirModule().CurFunction()->GetSymTab()->GetSymbolFromStIdx(stIdx.Idx());
+      ASSERT(symAddrSym != nullptr, "null ptr check");
       std::string str;
       if (CGOptions::IsArm64ilp32()) {
         str = ".word";
@@ -3499,6 +3492,7 @@ void Emitter::SetupDBGInfo(DebugInfo *mirdi) {
         die->SetAttr(DW_AT_frame_base, exprloc);
       } break;
       case DW_TAG_structure_type:
+      case DW_TAG_union_type:
       case DW_TAG_class_type:
       case DW_TAG_interface_type: {
         DBGDieAttr *byteSizeAttr = LFindDieAttr(die, DW_AT_byte_size);
