67995214ebdd1aed90eb63c350c28e3d945b6cc3
binaryfz <fuzhou@huawei.com>
2022-06-20 09:44:26 +0800

    delete useless folders

--- src/MapleFE/test/java/openjdk/Class-3.java
+++
@@ -1,9 +0,0 @@
-public final class Class<T> {
-    public Method[] getDeclaredMethods() {
-        Method[] result = getDeclaredMethodsUnchecked(false);
-        for (Method m : result) {
-            m.getParameterTypes();
-        }
-        return result;
-    }
-}
