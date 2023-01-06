30c438bc79e697dd7a23a1af78c36cc6cd19b36c
binaryfz <fuzhou@huawei.com>
2023-01-06 09:45:49 +0800

    [mapleall]Internal commit msg:     [maplebe]tailcall fixbug

--- src/mapleall/maple_be/include/cg/tailcall.h
+++ src/mapleall/maple_be/include/cg/tailcall.h
@@ -81,6 +81,8 @@ class TailCallOpt {
   bool stackProtect = false;
   MapleMap<BB*, MapleSet<Insn*>> exitBB2CallSitesMap;
   BB *curTailcallExitBB = nullptr;
+  bool isTailCallOther = false;
+  bool isTailCallSelf = false;
 };
 
 MAPLE_FUNC_PHASE_DECLARE_BEGIN(CgTailCallOpt, maplebe::CGFunc)
